f332cbb1f951378d1eb66196a764cbe39f105be2
David Carrillo Castillo <104275645+davidcarrillo10288@users.noreply.github.com>
2024-10-03 01:44:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+# from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-from collections import OrderedDict
+# from collections import OrderedDict
 # from streamlit_chromadb_connection.chromadb_connection import ChromaDBConnection  # Importa ChromaDBConnection
 from langchain_chroma import Chroma
 
